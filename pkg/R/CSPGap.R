639df049bbb5a676d691f2c237f5e3da71328e5b
wotuzu17 (Andreas Voellenklee) <wotuzu17@gmail.com>
2012-01-12 19:41:06 +0000

    added help topics
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/candlesticks@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/CSPGap.R
+++ pkg/R/CSPGap.R
@@ -1,15 +1,19 @@
 CSPGap <- function (TS, ignoreShadows=FALSE) {
-  if (!is.OHLC(TS)) {
-    stop("Price series must contain Open, High, Low and Close.")
-  }
-  LAGTS <- LagOHLC(TS, k=1)
   if (ignoreShadows==TRUE) {
+    if (!is.OC(TS)) {
+      stop("Price series must contain Open and Close.")
+    }
+    LAGTS <- LagOC(TS, k=1)
     TSOC <- cbind(Op(TS), Cl(TS))
     LAGTSOC <- cbind(Op(LAGTS), Cl(LAGTS))
     UPGAP <- eval(as.xts(apply(LAGTSOC,1,max)) < as.xts(apply(TSOC,1,min)))
     DOWNGAP <- eval(as.xts(apply(LAGTSOC,1,min)) > as.xts(apply(TSOC,1,max)))    
   }
   else {
+    if (!is.OHLC(TS)) {
+      stop("Price series must contain Open, High, Low and Close.")
+    }
+    LAGTS <- LagOHLC(TS, k=1)
     UPGAP <- eval(Lo(TS) > Hi(LAGTS))
     DOWNGAP <- eval(Hi(TS) < Lo(LAGTS))
   }
