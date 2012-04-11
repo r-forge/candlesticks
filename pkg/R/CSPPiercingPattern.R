8cb62088e56a7165a1c4e73512d09b60f97afcef
wotuzu17 (Andreas Voellenklee) <wotuzu17@gmail.com>
2012-04-11 13:53:38 +0000

    re-worked detection rules for Piercing Pattern and Dark Cloud Cover
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/candlesticks@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/CSPPiercingPattern.R
+++ pkg/R/CSPPiercingPattern.R
@@ -1,12 +1,15 @@
-CSPPiercingPattern <- function(TS) {
+CSPPiercingPattern <- function(TS, n=20, minbodysizeMedian=1) {
   if (!is.OC(TS)) {
     stop("Price series must contain Open and Close.")
   }
   LAGTS <- LagOC(TS, k=1)
+  LongCandleBody <- CSPLongCandleBody(LAGTS, n=n, threshold=minbodysizeMedian)
+
   PiercingPattern <- reclass(eval (
-    Op(LAGTS)>Cl(LAGTS) & Cl(TS)>Op(TS)
-    & Cl(LAGTS)>Op(TS) & Cl(TS)>(Op(LAGTS)+Cl(LAGTS))/2
-    & Op(LAGTS)>Cl(TS) ), TS)
+    LongCandleBody[,"LongBlackCandleBody"] & # first candle is black and longer than median of past n candles
+    Op(TS)<Cl(LAGTS) &                 # second candle opens lower than close of 1st candle
+    Cl(TS)>=(Op(LAGTS)+Cl(LAGTS))/2 &  # second candle closes at or higher than half of 1st candles' body
+    Cl(TS)<Op(LAGTS) ), TS)            # close of second candle is lower than open of 1st candle
   colnames(PiercingPattern) <- c("PiercingPattern")
   xtsAttributes(PiercingPattern) <- list(bars=2)
   return(PiercingPattern)
